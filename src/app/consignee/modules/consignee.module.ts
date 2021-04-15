dc0ef0d5701c5c4b132cfba0835ec93fcb2006d4
Jitiendran <jitiendranjiji2000@gmail.com>
2021-04-15 13:28:26 +0530

    added timeline

--- src/app/consignee/modules/consignee.module.ts
+++ src/app/consignee/modules/consignee.module.ts
@@ -1,13 +1,14 @@
 import { AppRoutingModule } from "./../../app-routing.module";
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
+import { MatStepperModule } from "@angular/material/stepper";
 
 import { ConsigneeComponent } from "../consignee.component";
 import { DashbordComponent } from "../dashbord/dashbord.component";
 import { OrdersComponent } from "../orders/orders.component";
 import { PaymentComponent } from "../payment/payment.component";
 import { StatusComponent } from "../status/status.component";
-import { MatStepperModule } from "@angular/material/stepper";
+import { MzdTimelineModule } from "ngx-mzd-timeline";
 @NgModule({
   declarations: [
     ConsigneeComponent,
@@ -16,6 +17,11 @@ import { MatStepperModule } from "@angular/material/stepper";
     StatusComponent,
     PaymentComponent,
   ],
-  imports: [CommonModule, AppRoutingModule, MatStepperModule],
+  imports: [
+    CommonModule,
+    AppRoutingModule,
+    MatStepperModule,
+    MzdTimelineModule,
+  ],
 })
 export class ConsigneeModule {}
--- src/app/consignee/status/status.component.ts
+++ src/app/consignee/status/status.component.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit } from "@angular/core";
 @Component({
-  selector: 'app-status',
-  templateUrl: './status.component.html',
-  styleUrls: ['./status.component.scss']
+  selector: "app-status",
+  templateUrl: "./status.component.html",
+  styleUrls: ["./status.component.scss"],
 })
 export class StatusComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  iconArray = [
+    "fas fa-check",
+    "fas fa-box",
+    "fas fa-dolly-flatbed",
+    "fas fa-shipping-fast",
+  ];
+  orderStatus: any = [
+    {
+      Status: "Ordered",
+      Time: "Thu Apr 15 1:03PM",
+    },
+    {
+      Status: null,
+      Time: null,
+    },
+    {
+      Status: null,
+      Time: null,
+    },
+    {
+      Status: null,
+      Time: null,
+    },
+  ];
+  constructor() {}
+  ngOnInit(): void {}
 }
