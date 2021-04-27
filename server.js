04835aed073386afcf35790ad5595b369e917b3c
Jitiendran <jitiendranjiji2000@gmail.com>
2021-04-27 11:08:57 +0530

    added server.js for deployment

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const path = require("path");
+
+const app = express();
+
+app.use(express.static(__dirname + "/dist/lightspeed"));
+
+app.get("*", (req, res, next) => {
+  res.sendFile(path.join(__dirname) + "/dist/lightspeed/index.html");
+});
+
+app.listen(process.env.PORT || 8080);
